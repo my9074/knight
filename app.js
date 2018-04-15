8bcb26c30168579b81c3fff408da866d1fa0495a
my9074 <313231068@qq.com>
2018-04-15 21:53:11 +0800

    feat(Knight): Encapsulation knight

--- app.js
+++ app.js
@@ -1,19 +1,7 @@
-const koa = require('koa');
+const knight = require('./knight/core');
+const app = new knight();
 
-const setRouters = require('./routerLoader');//引入router中间件
-
-const controllers = require('./loader').loadController();//引入router中间件
-
-koa.prototype['controller'] = {};
-controllers.forEach(ctrl => {
-    koa.prototype.controller[ctrl.name] = ctrl.module;
-})
-
-const app = new koa();
-
-
-app.use(setRouters(app));//引入router中间件
-
+app.setRouters();
 app.listen(3000, '127.0.0.1', () => {
     console.log('服务器启动');
 })
