518246b0f618f78c49c7ddb25e6f8fa6f67d1708
my9074 <313231068@qq.com>
2018-04-17 09:48:13 +0800

    perf(Loader): Split the loader into a separate class.

--- knight/core.js
+++ knight/core.js
@@ -1,44 +1,12 @@
 const koa = require('koa');
-const fs = require('fs');
 const koaRoute = require('koa-router');
-
-class KnightLoader {
-    removeString(source) {
-        const string = 'knight';
-        const index = source.lastIndexOf(string);
-        const len = string.length;
-        return source.substring(0, index);
-    }
-
-    loader(path) {
-        const dir = fs.readdirSync(path);
-        return dir.map((filename) => {
-            const module = require(path + '/' + filename);
-            return { name: filename.split('.')[0], module };
-        })
-    }
-
-    loadController() {
-        const url = this.removeString(__dirname) + '/controller';
-        return this.loader(url);
-    }
-
-    loadService() {
-        const url = this.removeString(__dirname) + '/service';
-        return this.loader(url);
-    }
-
-    loadConfig() {
-        const url = this.removeString(__dirname) + '/config';
-        return this.loader(url);
-    }
-}
+const Loader = require('./loader')
 
 class Knight extends koa {
     constructor(props) {
         super(props);
         this.router = new koaRoute();
-        this.loader = new KnightLoader();
+        this.loader = new Loader();
         this.controller = {};
         this.config = {};
 
